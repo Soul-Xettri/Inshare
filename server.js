014fcba1faf60a130b4a54095e48a18c38352402
Soul-Xettri <rakeshchhetri420@gmail.com>
2023-06-15 16:13:54 +0545

    Inshare

---
+++ server.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const path = require('path');
+const cors = require('cors');
+
+app.use(cors(corsOptions))
+app.use(express.static('public'));
+
+const connectDB = require('./config/db');
+connectDB();
+
+app.use(express.json());
+
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
+// Routes 
+app.use('/api/files', require('./routes/files'));
+app.use('/files', require('./routes/show'));
+app.use('/files/download', require('./routes/download'));
+
+
+app.listen(PORT, console.log(`Listening on port ${PORT}.`));
